5c2fdcdbca92670d6f03cd10b96b9395c71710ad
muhammadsajid3674 <sajiddark3674@gmail.com>
2023-02-12 17:59:55 +0500

    weather app with node express https bodyParse

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/' , (req, res) => {
-    res.send('<h1>Hello World</h1>')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const result = num1 + num2;
+    res.send('The result is ' + result)
+})
+
+app.get('/bmiCalculator', (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
+app.post('/bmiCalculator', (req, res) => {
+    console.log(req.body);
+    const weight = parseFloat(req.body.weight);    
+    const height = parseFloat(req.body.height);
+    const result = weight / (height * height);
+    res.send('Your BMI is ' + result)
 })
 
 app.listen(3000, () => {
---
+++ WeatherProject/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    const query = req.body.cityName;
+    const apiKey = "1b7689d45df4afdf9c345482e797cf55";
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=' + apiKey + '&units=metric';
+    https.get(URL, (response) => {
+        console.log(response.statusCode);
+        response.on("data", (data) => {
+            const weatherUpdates = JSON.parse(data);
+            const temp = weatherUpdates.main.temp;
+            const weatherDescription = weatherUpdates.weather[0].description;
+            const icon = weatherUpdates.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>The temperature in " + query + "  is " + temp + " degree Celsius.</h1>")
+            res.write("<p>The weather is currently " + weatherDescription + "</p>")
+            res.write("<img src=" + imageURL + " alt=" + weatherDescription + "/>")
+            res.send();
+        })
+    });
+})
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
